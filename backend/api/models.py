8b20531570b02e6b7d245896ce9e7b361f46b936
Paul Harrington <hello@loosenthedark.tech>
2026-05-19 15:27:11 +0100

    BACKEND: Create model & serializer for notes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    # Ensures this field gets automatically populated, i.e. we don't want to be passing a value for it
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Establishes a one-to-many r'ship between a user and his/her note(s)
+    # NB: Based on val passed to on_delete arg below, whenever a user is removed from DB all notes associated with that user will also be deleted
+    # NB: We can now access a user's notes via the notes field on the user object
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notes"
+    )
+
+    def __str__(self):
+        return self.title
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from .models import Note
 from rest_framework import serializers
 
 
@@ -8,9 +9,17 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         # All the fields we want to serialise when accepting AND returning a new user
         fields = ["id", "username", "password"]
-        # Tells Django not to return user PWs with rest of new user data (i.e. PWs will only be accepted - not returned)
+        # Tells Django not to return user PWs with rest of new user data (i.e. PWs will only be accepted - not returned... "We can set but not see")
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "body", "created_at", "author"]
+        # Ensures we cannot "assign" a note to any user other than the one currently authenticated (i.e. "We can see but not set")
+        extra_kwargs = {"author": {"read_only": True}}
