3e6e65a5a1ce2740a7095598c8d1be2196f1a6ad
Paul Harrington <hello@loosenthedark.tech>
2026-05-19 17:24:57 +0100

    FRONTEND: Write initial routing stub code (with protected home route)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,52 @@
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+
+import ProtectedRouteComponent from "./components/ProtectedRoute";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+import PageNotFoundPage from "./pages/NotFound";
+
+const Logout = () => {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+};
+
+// Cleanup mechanism required to prevent stale tokens (associated with other user(s)) from being passed when trying to register a new user
+const LogoutThenRegister = () => {
+  localStorage.clear();
+  return <RegisterPage />;
+};
+
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <ProtectedRouteComponent>
+        <HomePage />
+      </ProtectedRouteComponent>
+    ),
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <LogoutThenRegister />,
+  },
+  // Fallback route
+  {
+    path: "*",
+    element: <PageNotFoundPage />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-    </>
-  )
+  return <RouterProvider router={browserRouter} />;
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,4 @@
+const NotFound = () => {
+  return <div>404 page</div>;
+};
+export default NotFound;
