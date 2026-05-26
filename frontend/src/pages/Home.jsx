f8e0504aa2e2e4ee4023a72c97ac582f0bf2869b
Paul Harrington <hello@loosenthedark.tech>
2026-05-26 14:43:04 +0100

    FRONTEND: Build out Home component UI & CR(U)D functionality for creating new Notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,86 @@
+import { useEffect, useState } from "react";
+
+import api from "../api";
+
 const Home = () => {
-  return <div>Home page</div>;
+  const [notes, setNotes] = useState([]);
+  const [newNoteTitle, setNewNoteTitle] = useState("");
+  const [newNoteBody, setNewNoteBody] = useState("");
+
+  const fetchNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((notesData) => {
+        setNotes(notesData);
+        console.log(notesData);
+      })
+      .catch((error) => alert(error));
+  };
+
+  const createNewNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { title: newNoteTitle, body: newNoteBody })
+      .then((res) => {
+        if (res.status === 201) {
+          alert("Note successfully created!");
+        } else {
+          alert("Failed to create note... Please try again!");
+        }
+        fetchNotes();
+      })
+      .catch((error) => alert(error));
+  };
+
+  const deleteNote = (noteId) => {
+    api
+      .delete(`api/note/delete/${noteId}/`)
+      .then((res) => {
+        if (res.status === 204) {
+          alert("Note successfully deleted!");
+        } else {
+          alert("Failed to delete note... Please try again!");
+        }
+        fetchNotes();
+      })
+      .catch((error) => alert(error));
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h2>Notes</h2>
+      </div>
+      <h2>Create a new Note...</h2>
+      <form onSubmit={createNewNote}>
+        <label htmlFor="note-title">Title:</label>
+        <br />
+        <input
+          id="note-title"
+          name="noteTitle"
+          required
+          value={newNoteTitle}
+          onChange={(e) => setNewNoteTitle(e.target.value)}
+        />
+        <br />
+        <label htmlFor="note-body">Body:</label>
+        <br />
+        <textarea
+          id="note-body"
+          name="noteBody"
+          required
+          value={newNoteBody}
+          onChange={(e) => setNewNoteBody(e.target.value)}
+        />
+        <br />
+        <input type="submit" value="Create Note" />
+      </form>
+    </div>
+  );
 };
 export default Home;
